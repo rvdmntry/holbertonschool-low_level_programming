331e20e9a4b30f18d049a8eb5f2616eaa6c68c8f
Anto Vega <antovega@Antos-MBP.localdomain>
2024-02-21 13:08:29 -0400

    alphabet

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,21 +1,16 @@
-#include"main.h"
-
+#include "_putchar.c"
 /**
  * main - Entry point
  *
- * Description: prints _putchar using putchar prototype
- *
- * Return: Always 0 (Success)
-*/
-
+ * Return: Alway 0 (Sucess)
+ */
 int main(void)
 {
-	char str[] = "_putchar";
-	int ch;
-
-	for (ch = 0; ch < 8; ++ch)
-		_putchar(str[ch]);
-	_putchar('\n');
-
+	char *h = "_putchar\n";
+	while (*h)
+	{
+		_putchar(*h);
+		h++;
+	}
 	return (0);
 }
