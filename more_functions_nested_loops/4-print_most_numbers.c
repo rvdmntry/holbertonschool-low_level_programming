e1ec61b88201e7a6431b0ac71e37c6e7449b11ad
Anto Vega <antovega@Antos-MBP.localdomain>
2024-02-22 14:54:49 -0400

    mostnumber

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - prints the numbers, from 0 to 9, followed by a new line.
+ * Do not print 2 and 4.
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9; num++)
+	{
+		if (num != 2 && num != 4)
+		{
+			_putchar(num + '0');
+		}
+	}
+	_putchar('\n');
+}
---
+++ more_functions_nested_loops/cat
@@ -0,0 +1,2 @@
+void print_most_numbers(void);
+int _putchar(char c);
