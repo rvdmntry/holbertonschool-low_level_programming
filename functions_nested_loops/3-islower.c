508341e04d5b92ffdd69d9e2da159ba6c60e68df
Anto Vega <antovega@Antos-MacBook-Pro.local>
2024-02-22 22:45:44 -0400

    islower

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -2,17 +2,13 @@
 
 /**
  * _islower - checks for lowercase character
- * @c: the character to check
+ * @c: character to be checked
  * Return: 1 if c is lowercase, 0 otherwise
  */
 int _islower(int c)
 {
-	if (c >= 'a' && c <= 'z')
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
+if (c >= 97 && c <= 122)
+return (1);
+else
+return (0);
 }
