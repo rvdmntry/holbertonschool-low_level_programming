f1f5cf2604cb761d7435443d74e44ce32c7c7630
Anto Vega <antovega@Antos-MacBook-Pro.local>
2024-02-22 22:29:17 -0400

    isalpha

--- functions_nested_loops/4-isalpha.c
+++ functions_nested_loops/4-isalpha.c
@@ -1,18 +1,14 @@
 #include "main.h"
 
 /**
- * _isalpha - checks for an alphabetic character
- * @c: the character to be checked
- * Return: 1 if c is a letter, lowercase or uppercase, 0 otherwise
+ * _isalpha - checks for alphabetic character
+ * @c: character to be checked
+ * Return: 1 if c is a letter, 0 otherwise
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
-	{
-		return 1;
-	}
-	else
-	{
-		return 0;
-	}
+if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122))
+return (1);
+else
+return (0);
 }
