f9e9ad1da5335d6117babcb884869462738630b3
Anto Vega <antovega@Antos-MacBook-Pro.local>
2024-02-22 21:28:35 -0400

    tirangles

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,32 +1,28 @@
 #include "main.h"
-
 /**
- * print_triangle - prints a triangle, followed by a new line.
- * @size: the size of the triangle.
- */
+*print_triangle - prints a triangle
+*@size:size parameter of triangle
+*Return:returns nothing
+*/
 void print_triangle(int size)
 {
-	int row, column, spaces;
+int inc1, inc2;
 
-	if (size <= 0)
-	{
-		_putchar('\n');
-	}
-	else
-	{
-		for (row = 1; row <= size; row++)
-		{
-			// Print spaces
-			for (spaces = size - row; spaces > 0; spaces--)
-			{
-				_putchar(' ');
-			}
-			// Print hashes
-			for (column = 1; column <= row; column++)
-			{
-				_putchar('#');
-			}
-			_putchar('\n');
-		}
-	}
+if (size > 0)
+{
+for (inc1 = 1; inc1 <= size; inc1++)
+{
+for ((inc2 = size - inc1); inc2 > 0; inc2--)
+_putchar(' ');
+
+for (inc2 = 0; inc2 < inc1; inc2++)
+_putchar('#');
+
+if (inc1 == size)
+continue;
+
+_putchar('\n');
+}
+}
+_putchar('\n');
 }
