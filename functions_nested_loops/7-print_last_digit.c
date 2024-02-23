41555ca6dec1f7928c2cdfa93503221ce8d621af
Anto Vega <antovega@Antos-MacBook-Pro.local>
2024-02-22 22:09:11 -0400

    print

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -2,17 +2,16 @@
 
 /**
  * print_last_digit - prints the last digit of a number
- * @n: the number to retrieve the last digit from
- * Return: The value of the last digit
+ * @n: number to be checked
+ * Return: last digit of n
  */
 int print_last_digit(int n)
 {
-	int last_digit = n % 10;
+int last_digit;
 
-	if (last_digit < 0)
-	{
-		last_digit = -last_digit;
-	}
-	_putchar('0' + last_digit);
-	return last_digit;
+last_digit = n % 10;
+if (last_digit < 0)
+last_digit = -last_digit;
+_putchar(last_digit + '0');
+return (last_digit);
 }
