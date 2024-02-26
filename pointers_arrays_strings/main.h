5b8aced7c1aadc93925cd03a10e3c586d6ef570d
Anto Vega <antovega@Antos-MBP.localdomain>
2024-02-26 11:16:20 -0400

    puts

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -5,7 +5,6 @@ void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 int _strlen(char *s);
 void _puts(char *str);
-void _puts(char *str);
-int _putchar(char c); // Assuming _putchar is provided elsewhere
+int _putchar(char c);
 
 #endif /* MAIN_H */
