b874dc47401f3e5d8abbb9f8e2fa9166bf1b995f
Anto Vega <antovega@Antos-MBP.localdomain>
2024-02-26 10:25:06 -0400

    strlen

--- pointers_arrays_strings/2-strlen.c
+++ pointers_arrays_strings/2-strlen.c
@@ -1,13 +1,18 @@
 #include "main.h"
 
 /**
- * swap_int - swaps the values of two integers
- * @a: pointer to the first integer
- * @b: pointer to the second integer
+ * _strlen - returns the length of a string
+ * @s: string to evaluate
+ * Return: the length of the string
  */
-void swap_int(int *a, int *b)
+int _strlen(char *s)
 {
-	int temp = *a;
-	*a = *b;
-	*b = temp;
+	int length = 0;
+
+	while (s[length] != '\0')
+	{
+		length++;
+	}
+
+	return length;
 }
