e2e22dd8a42a4eb3844ab0940b16611174814949
Anto Vega <antovega@Antos-MBP.localdomain>
2024-02-29 13:51:56 -0400

    print_chessboard

---
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_chessboard - prints the chessboard
+ * @a: two-dimensional array representing the chessboard
+ *
+ * Return: Nothing.
+ */
+void print_chessboard(char (*a)[8])
+{
+	int i, j;
+
+	for (i = 0; i < 8; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			_putchar(a[i][j]);
+		}
+		_putchar('\n');
+	}
+}
+
+int _putchar(char c)
+{
+	return write(1, &c, 1);
+}
