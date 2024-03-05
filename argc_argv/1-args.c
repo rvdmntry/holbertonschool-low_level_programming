58e317cabb0896dad325fd0b01fff7aa580b0343
Anto Vega <antovega@Antos-MacBook-Pro.local>
2024-03-05 11:17:22 -0400

    args

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -4,7 +4,7 @@
  * main - Prints the number of command line arguments passed.
  * @argc: The number of command line arguments.
  * @argv: An array of pointers to the command line arguments.
- *        We won't use argv in this program, hence the attribute to avoid warnings.
+ * We won't use argv in this program, hence the attribute to avoid warnings.
  * Return: 0 on success.
  */
 int main(int argc, char *argv[] __attribute__((unused)))
