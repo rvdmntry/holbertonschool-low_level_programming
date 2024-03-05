2198ecada53cffb80bccf39a355e5202138b65b1
Anto Vega <antovega@Antos-MacBook-Pro.local>
2024-03-05 15:35:57 -0400

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -4,17 +4,22 @@
 
 /**
  * main - Adds positive numbers.
- * @argc: The number of command line arguments.
- * @argv: An array of pointers to the command line arguments.
+ * @argc: Number of command line arguments.
+ * @argv: Array containing the command line arguments.
+ * Description: Adds positive numbers provided as command line arguments.
+ * Prints the result followed by a new line. If no number is passed,
+ * prints 0 followed by a new line. If one of the numbers contains symbols
+ * that are not digits, prints "Error", followed by a new line, and returns 1.
  * Return: 0 on success, 1 if an argument contains non-digit characters.
  */
 int main(int argc, char *argv[])
 {
 int sum = (0);
+int i, j;
 
-for (int i = 1; i < argc; i++)
+for (i = 1; i < argc; i++)
 {
-for (int j = 0; argv[i][j]; j++)
+for (j = 0; argv[i][j]; j++)
 {
 if (!isdigit(argv[i][j]))
 {
