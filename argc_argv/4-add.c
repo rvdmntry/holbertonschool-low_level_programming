a4d5c0494e1d41c3c1c36bafafb22d7cdcdc3461
Anto Vega <antovega@Antos-MacBook-Pro.local>
2024-03-05 15:27:47 -0400

    add

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -2,30 +2,11 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-/** 
- * Checks if a string consists only of digits.:x
- * @str: The string to check.
- * Return: 1 if the string is all digits, 0 otherwise.
- */
-
-int is_number(char *str);
-{
-int i = (0);
-
-while (str[i])
-{
-if (!isdigit(str[i]))
-return (0);
-i++;
-}
-return (1);
-}
-
 /**
  * main - Adds positive numbers.
  * @argc: The number of command line arguments.
  * @argv: An array of pointers to the command line arguments.
- * Return: 0 on success, 1 if an argument is not a digit.
+ * Return: 0 on success, 1 if an argument contains non-digit characters.
  */
 int main(int argc, char *argv[])
 {
@@ -33,15 +14,15 @@ int sum = (0);
 
 for (int i = 1; i < argc; i++)
 {
-if (is_number(argv[i]))
+for (int j = 0; argv[i][j]; j++)
 {
-sum += atoi(argv[i]);
-}
-else
+if (!isdigit(argv[i][j]))
 {
 printf("Error\n");
 return (1);
 }
+}
+sum += atoi(argv[i]);
 }
 
 printf("%d\n", sum);
