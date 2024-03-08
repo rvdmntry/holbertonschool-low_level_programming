6e898d7732bd5fc60f24fe41bed79b711cb5b71a
Anto Vega <antovega@Antos-MacBook-Pro.local>
2024-03-07 21:11:36 -0400

    realloc

--- more_malloc_free/100-realloc.c
+++ more_malloc_free/100-realloc.c
@@ -23,7 +23,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 
 	if (!ptr)
-		return malloc(new_size);
+		return (malloc(new_size));
 
 	new_ptr = malloc(new_size);
 	if (!new_ptr)
