db92eec0487787b31f999c5de3a0b52b1b9caef7
Anto Vega <antovega@Antos-MBP-13.localdomain>
2024-03-12 11:01:58 -0400

    array_itirator

--- function_pointers/function_pointers.h
+++ function_pointers/function_pointers.h
@@ -2,9 +2,12 @@
 #define FUNCTION_POINTERS_H
 
 #include <stdlib.h>
+#include <stddef.h>
 
 void print_name(char *name, void (*f)(char *));
 void print_name_as_is(char *name);
 void print_name_uppercase(char *name);
+void print_name(char *name, void (*f)(char *));
+void array_iterator(int *array, size_t size, void (*action)(int));
 
 #endif /* FUNCTION_POINTERS_H */
