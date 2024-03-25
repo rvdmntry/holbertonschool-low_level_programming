f567b71ae020cf30c8fb3a8bfceb30b6411b142e
Anto Vega <antovega@Antos-MBP-13.localdomain>
2024-03-25 11:21:28 -0400

    printlist

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -4,7 +4,6 @@
 /**
  * print_list - Prints all the elements of a list_t list.
  * @h: A pointer to the head of the list_t list.
- *
  * Return: The number of nodes in the list.
  */
 size_t print_list(const list_t *h)
