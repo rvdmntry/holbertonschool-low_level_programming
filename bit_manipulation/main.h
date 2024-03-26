42e0fbd27bc576d43b9f4de490d21545e4634878
Anto Vega <antovega@Antos-MacBook-Pro-13.local>
2024-03-26 15:50:17 -0400

    binary_to_uint

--- bit_manipulation/main.h
+++ bit_manipulation/main.h
@@ -1,1 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdlib.h>
+
+/* Function Prototypes */
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: string containing the binary number
+ * Return: the converted number, or 0 if there is an error
+ */
 unsigned int binary_to_uint(const char *b);
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
+ */
+int _putchar(char c);
+
+#endif /* MAIN_H */
