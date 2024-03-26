2e854f7b5463a5129d8390d445ff50f02dfd3ec8
Anto Vega <antovega@Antos-MacBook-Pro-13.local>
2024-03-26 14:21:35 -0400

    lists.hupdate

--- doubly_linked_lists/1-dlistint_len.c
+++ doubly_linked_lists/1-dlistint_len.c
@@ -3,18 +3,17 @@
 /**
  * dlistint_len - returns the number of elements in a linked dlistint_t list.
  * @h: pointer to the head of the doubly linked list.
- *
  * Return: the number of elements in the list.
  */
 size_t dlistint_len(const dlistint_t *h)
+
 {
 	size_t count = 0;
 
-	while (h != NULL)
-	{
-	count++;
-	h = h->next;
-	{
+	while (h != NULL) {
+		count++;
+		h = h->next;
+	}
 
 	return (count);
 }
