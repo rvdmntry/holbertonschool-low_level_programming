33f4d1531f5b4597c2e62e46f7f3113158f155a9
Anto Vega <antovega@Antos-MacBook-Pro-13.local>
2024-03-26 14:37:07 -0400

    adddnodeintupdate

--- doubly_linked_lists/2-add_dnodeint.c
+++ doubly_linked_lists/2-add_dnodeint.c
@@ -7,18 +7,20 @@
  * @n: the integer value for the new node.
  * Return: the address of the new element, or NULL if it failed.
  */
-dlistint_t *add_dnodeint(dlistint_t **head, const int n) {
+dlistint_t *add_dnodeint(dlistint_t **head, const int n)
+{
 	dlistint_t *new_node = malloc(sizeof(dlistint_t));
+
 	if (new_node == NULL)
 		return (NULL);
 
 	new_node->n = n;
 	new_node->prev = NULL;
-	new_node->next = *head; // Next of new node will be the current head
+	new_node->next = *head;
 
 	if (*head != NULL)
-		(*head)->prev = new_node; // Update previous head if list is not empty
+		(*head)->prev = new_node;
 
-	*head = new_node; // Update the head to be the new node
+	*head = new_node;
 	return (new_node);
 }
