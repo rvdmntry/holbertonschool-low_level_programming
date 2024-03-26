07b06d5b3ca83be23c8d38f5d243fad42c03f52e
Anto Vega <antovega@Antos-MacBook-Pro-13.local>
2024-03-26 16:19:39 -0400

    flip_bits

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,7 +1,8 @@
 #include "main.h"
 
 /**
- * flip_bits - Counts the number of bits to flip to get from one number to another.
+ * flip_bits - Counts the number of bits to flip
+ * to get from one number to another.
  * @n: The first number.
  * @m: The second number.
  * Return: The number of bits you would need to flip to get from n to m.
