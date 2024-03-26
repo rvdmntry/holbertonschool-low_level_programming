c01ec232f685ee0d754d8131cebc50316fa555a2
Anto Vega <antovega@Antos-MacBook-Pro-13.local>
2024-03-26 09:16:14 -0400

    printdlistint

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -7,10 +7,12 @@
  *
  * Return: the number of nodes in the list.
  */
-size_t print_dlistint(const dlistint_t *h) {
+size_t print_dlistint(const dlistint_t *h)
+{
 	size_t nodes = 0;
 
-	while (h != NULL) {
+	while (h != NULL)
+	{
 		printf("%d\n", h->n);
 		h = h->next;
 		nodes++;
